099bcdbd343dcba269d6a9ce1b0f1b57de266c03
Jeongyong Shin <j.y.shin@samsung.com>
2021-03-27 11:22:21 +0900

    add project C in order to test just build c++ to make static lib

---
+++ ProjectC/app/src/main/cpp/MinusCalc.h
@@ -0,0 +1,16 @@
+#ifndef PROJECTA_MINUSCALC_H
+#define PROJECTA_MINUSCALC_H
+
+class MinusCalc {
+public:
+    explicit MinusCalc( );
+    ~MinusCalc();
+    int calcMinus(int a);
+    int calcPlus(int b);
+private:
+
+};
+
+
+
+#endif //PROJECTA_MINUSCALC_H
